b969b29bd9caf0abc753d209a5028ace2f669825
jpschuh19 <83608880+jpschuh19@users.noreply.github.com>
2021-10-20 22:55:32 -0400

    added print_forward() for regular sorting, and added print_reverse() for reverse sorting

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ for line in sys.stdin:
 names.sort()
 names.sort(key=len, reverse=True)
 
-for i in names:
-    print_to_stdout(names[i])
+def print_forward():
+    for i in names:
+        print_to_stdout(names[i])
+
+def print_reverse():
+    i=len(names)-1
+    while(i>=0):
+        print_to_stdout(names[i])
+        i+=1
+
