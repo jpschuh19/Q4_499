c6816780158733f644718ae5fefb8c67adc27518
jpschuh19 <83608880+jpschuh19@users.noreply.github.com>
2021-10-27 23:50:59 -0400

    added the check methods for both the forward and reverse sort methods

--- main.py
+++ main.py
@@ -9,16 +9,57 @@ for line in sys.stdin:
     with open(line) as f:
         names = f.readlines()
 
+forward_list = []
+reverse_list = []
+
+unsorted = names
+
 names.sort()
 names.sort(key=len, reverse=True)
 
 def print_forward():
     for i in names:
+        forward_list.append(names[i])
         print_to_stdout(names[i])
 
 def print_reverse():
     i=len(names)-1
     while(i>=0):
         print_to_stdout(names[i])
+        reverse_list.append(names[i])
         i+=1
 
+def check_forward():
+    x=0
+    if(forward_list == unsorted):
+        for i in forward_list:
+            for j in unsorted:
+                if(forward_list[i] != unsorted[j]):
+                    print("Sorted list is NOT the same")
+                    x=1
+    else:
+        print("Sorted list is NOT the same")
+        x=1
+    if(x==0):
+        print("Sorted list is the same")
+
+def check_reverse():
+    x=0
+    if(reverse_list == unsorted):
+        for i in reverse_list:
+            for j in unsorted:
+                if (forward_list[i] != unsorted[j]):
+                    print("Sorted list is NOT the same")
+                    x=1
+    else:
+        print("Sorted list is NOT the same")
+        x=1
+    if(x==0):
+        print("Sorted list is the same")
+
+print_forward()
+check_forward()
+
+
+print_reverse()
+check_reverse()
